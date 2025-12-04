01f41a5dc24f25ba524548706713b92baf6723e0
Alex <calexvillota@gmail.com>
2025-12-04 02:16:03 -0600

    stuff

--- Painter.js
+++ Painter.js
@@ -11,7 +11,7 @@ export class Painter{
     //create painting texture
     let texture = new T.TextureLoader().load("./textures/empty.png");
     let paintMat = shaderMaterial("./shaders/paint.vs", "./shaders/paint.fs", {
-        uniforms: {tex: {value: texture}, point: {value: new T.Vector2(-10, -10)}, size: {value: 0.1}}
+        uniforms: {tex: {value: texture}, point: {value: new T.Vector2(-10, -10)}, point2: {value: new T.Vector2(-10, -10)}, point3: {value: new T.Vector2(-10, -10)}, point4: {value: new T.Vector2(-10, -10)}, point5: {value: new T.Vector2(-10, -10)}, size: {value: 0.1}}
     });
 
     
@@ -51,6 +51,10 @@ export class Painter{
       });
     this.debugPlane =  new T.Mesh(geometry, debugMat)
     this.debugPlane.translateY(6)
+    this.points = []
+    this.target = null
+    this.empty = new T.Vector2(-10, -10)
+    this.power = 1
   }    
   
   /**
@@ -58,67 +62,84 @@ export class Painter{
    * @param {*} delta 
    * @param {*} timeOfDay 
    */
-  paint(position, direction, delta) {
-    
-    this.lastPaint += delta
-    if(this.lastPaint > 20){
-        this.lastPaint = 0
-        this.raycaster.set(position, direction)
+  paint() {
 
-        let intersects = this.raycaster.intersectObjects(this.paintables, true)
-            // If the ray hits an object, log the intersection
-        if (intersects.length > 0 && intersects[0].object.material.uniforms.tex.value.image && this.mat.uniforms.tex.value.image) {
-            if(intersects[0].object.material.uniforms.isClean.value){   
-                return {name: intersects[0].object.name, dirtiness: 100}
-            }
-
-            if(intersects[0].object.size){
-                this.mat.uniforms.size.value = intersects[0].object.size
-            }
-            else{
-                this.mat.uniforms.size.value = .2
-            }
-            let oldSize = new T.Vector2()
-            this.renderer.getSize(oldSize)
-            this.mat.uniforms.point.value = intersects[0].uv
-            this.renderer.copyTextureToTexture(intersects[0].object.material.uniforms.dirty.value, this.mat.uniforms.tex.value)
-
-            this.renderer.setSize(512, 512)
-            this.renderer.setRenderTarget(this.renderTarget)
-            this.renderer.render(this.renderScene, this.camera)
-
-            //how much left?
-            const pixels = new Uint8Array(512 * 512 * 4);  // *4 because RGBA, 4 per pixel
-            this.renderer.readRenderTargetPixels(this.renderTarget, 0, 0, 512, 512, pixels);
-
-            let totalRed = 0;
-
-            // Red is dirty mask, all we need
-            for (let i = 0; i < pixels.length; i += 4) {
-                const red = pixels[i];
-                totalRed += red;
-            }
-
-            const possibleRed = (pixels.length / 4) * 255
-            const redness = totalRed / possibleRed * 100;  // As a percentage
-
-            //based on start/end
-            let end = intersects[0].object.end ? intersects[0].object.end : 5
-            let start = intersects[0].object.start ? intersects[0].object.start : 57
-
-            this.renderer.setRenderTarget(null)
-            if(redness < end){
-                console.log(end, redness, "yeah science")
-                intersects[0].object.material.uniforms.isClean.value = true
-            }    
-            
-            this.renderer.copyTextureToTexture(this.renderTarget.texture, intersects[0].object.material.uniforms.dirty.value)
-            this.renderer.setSize(oldSize.x, oldSize.y)
-            let percentClean = (redness-start)/(end-start)*100
-            return {name: intersects[0].object.name, dirtiness: percentClean}
+        console.log(this.points)
+    if (this.points.length > 0 && this.target.material.uniforms.tex.value.image && this.mat.uniforms.tex.value.image) {
+        if(this.target.material.uniforms.isClean.value){   
+            return {name: this.target.name, dirtiness: 100}
         }
-        return null
+        if(this.target.size){
+            this.mat.uniforms.size.value = this.target.size * this.power
+        }
+        else{
+            this.mat.uniforms.size.value = .2 * this.power
+        }
+        let oldSize = new T.Vector2()
+        this.renderer.getSize(oldSize)
+        this.mat.uniforms.point.value = this.points[0]
+        if(this.points.length > 1) this.mat.uniforms.point2.value = this.points[1]
+        else this.mat.uniforms.point2.value = this.empty
+        if(this.points.length > 2) this.mat.uniforms.point3.value = this.points[2]
+        else this.mat.uniforms.point3.value = this.empty
+        if(this.points.length > 3) this.mat.uniforms.point4.value = this.points[3]
+        else this.mat.uniforms.point4.value = this.empty
+        if(this.points.length > 4) this.mat.uniforms.point5.value = this.points[4]
+        else this.mat.uniforms.point5.value = this.empty
+        
+
+        
+        this.renderer.copyTextureToTexture(this.target.material.uniforms.dirty.value, this.mat.uniforms.tex.value)
+
+        this.renderer.setSize(512, 512)
+        this.renderer.setRenderTarget(this.renderTarget)
+        this.renderer.render(this.renderScene, this.camera)
+
+        //how much left?
+        const pixels = new Uint8Array(512 * 512 * 4);  // *4 because RGBA, 4 per pixel
+        this.renderer.readRenderTargetPixels(this.renderTarget, 0, 0, 512, 512, pixels);
+
+        let totalRed = 0;
+
+        // Red is dirty mask, all we need
+        for (let i = 0; i < pixels.length; i += 4) {
+            const red = pixels[i];
+            totalRed += red;
+        }
+
+        const possibleRed = (pixels.length / 4) * 255
+        const redness = totalRed / possibleRed * 100;  // As a percentage
+
+        //based on start/end
+        let end = this.target.end ? this.target.end : 5
+        let start = this.target.start ? this.target.start : 57
+
+        this.renderer.setRenderTarget(null)
+        if(redness < end){
+            console.log(end, redness, "yeah science")
+            this.target.material.uniforms.isClean.value = true
+        }    
+        
+        this.renderer.copyTextureToTexture(this.renderTarget.texture, this.target.material.uniforms.dirty.value)
+        this.renderer.setSize(oldSize.x, oldSize.y)
+        let percentClean = (redness-start)/(end-start)*100
+        return {name: this.target.name, dirtiness: percentClean}
     }
+    return null
+  }
+  getPaintPoint(position, direction){
+    this.raycaster.set(position, direction)
+    let intersects = this.raycaster.intersectObjects(this.paintables, true)
+    if (intersects.length > 0 && (this.target == null || intersects[0].object == this.target)) {
+        this.points.push(intersects[0].uv)
+        this.target = intersects[0].object  
+    }
+    return null
+  }
+
+  clearPoints(){
+    this.points = []
+    this.target = null
   }
     
 }
--- shaders/paint.fs
+++ shaders/paint.fs
@@ -4,6 +4,11 @@
  // get the texture from the program
  uniform sampler2D tex;
  uniform vec2 point;
+ uniform vec2 point2;
+ uniform vec2 point3;
+ uniform vec2 point4;
+ uniform vec2 point5;
+ 
  uniform float size;
 
 void main()
@@ -11,6 +16,24 @@ void main()
     float xdist = v_uv.x - point.x;
     float ydist = v_uv.y - point.y;
     float dist = sqrt(xdist*xdist + ydist*ydist);
+    
+    xdist = v_uv.x - point2.x;
+    ydist = v_uv.y - point2.y;
+    dist = min(sqrt(xdist*xdist + ydist*ydist), dist);
+        
+    xdist = v_uv.x - point3.x;
+    ydist = v_uv.y - point3.y;
+    dist = min(sqrt(xdist*xdist + ydist*ydist), dist);
+        
+    xdist = v_uv.x - point4.x;
+    ydist = v_uv.y - point4.y;
+    dist = min(sqrt(xdist*xdist + ydist*ydist), dist);
+        
+    xdist = v_uv.x - point5.x;
+    ydist = v_uv.y - point5.y;
+    dist = min(sqrt(xdist*xdist + ydist*ydist), dist);
+
+
     float mixvalue = 1.-smoothstep(0., size, dist);
     gl_FragColor = mix(texture2D(tex, v_uv), vec4(0., 0., 0., 1.0), mixvalue);
 }
